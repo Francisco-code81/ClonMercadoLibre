85b8a33c511b81a9c6993af48d64255aa89b44ff
Francisco Vargas <nahuelrobles561@gmail.com>
2023-11-28 23:55:59 -0300

    Mercado Liebre finalizado

--- src/app.js
+++ src/app.js
@@ -2,10 +2,18 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
-app.listen(3000, () => console.log('Hola mundo'));
+app.listen(3000, () => console.log('Servidor abierto'));
 
 app.use(express.static("public"));
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/home.html'))
+});
+
+app.get('/login.html', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/login.html'))
+});
+
+app.get('/register.html', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/register.html'))
 });
